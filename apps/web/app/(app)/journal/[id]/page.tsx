b39443fce62ac73bb6e4cb7fc901e873cd448aff
Nishant <nishkmg@gmail.com>
2026-05-12 15:39:50 +0530

    chore: remove unused useMemo import from journal detail page

--- apps/web/app/(app)/journal/[id]/page.tsx
+++ apps/web/app/(app)/journal/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useMemo } from "react";
+import { useState } from "react";
 import { Icon } from '@/components/ui/icon';
 import { useParams, useRouter } from "next/navigation";
 import Link from "next/link";
